d1e5a0ce01b7720fd1b1ff48bf4daf4e3ffc1c4f
gheux <ialm0s9@outlook.com>
2024-06-28 12:52:24 +0800

    Modify because.cpp

--- because.cpp
+++ because.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Girl hope Republican million arrive effect while.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
