2a6040b37c3fdbfa03b0d9dcfb107a8df5dc82ac
gheux <ialm0s9@outlook.com>
2024-07-08 11:53:27 +0800

    Modify others.c

--- others.c
+++ others.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Interview manager work three artist born coach.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
