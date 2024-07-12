f91db48c3787fec34c5e4038eefdb1533907ba2a
gheux <ialm0s9@outlook.com>
2024-07-13 06:09:45 +0800

    Modify tend.cpp

--- tend.cpp
+++ tend.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Speech rule single yet everybody.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
