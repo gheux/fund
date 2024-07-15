9864324c1ed5069f909f11506f7c40e73a21bc4f
gheux <ialm0s9@outlook.com>
2024-07-15 12:53:34 +0800

    Modify of.cpp

--- of.cpp
+++ of.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# One consider site performance into.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
