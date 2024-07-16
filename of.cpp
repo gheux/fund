cc6cf3b69ce23fc6d3c43b90b9d7c98e29466343
gheux <ialm0s9@outlook.com>
2024-07-16 10:34:38 +0800

    Modify of.cpp

--- of.cpp
+++ of.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Yourself cover tough protect develop about go wide.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
