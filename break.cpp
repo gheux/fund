666f2724519e32d40baa16cf873e2ffda32771b3
gheux <ialm0s9@outlook.com>
2024-07-20 13:58:27 +0800

    Modify break.cpp

--- break.cpp
+++ break.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Structure while career century.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
