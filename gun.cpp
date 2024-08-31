2ee845df3b095cc66efd1e33eb7fe7279fb7a297
gheux <ialm0s9@outlook.com>
2024-08-31 12:26:12 +0800

    Modify gun.cpp

--- gun.cpp
+++ gun.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Lay Mrs manager tonight pretty economy.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
