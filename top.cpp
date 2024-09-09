4c76384022962e3122bcbb362cc0ed475d8c2b67
gheux <ialm0s9@outlook.com>
2024-09-09 21:23:56 +0800

    Modify top.cpp

--- top.cpp
+++ top.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Fire national job reduce list.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
