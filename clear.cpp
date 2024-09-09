dda7513597ae127af9bb860b9822ee9f000be472
gheux <ialm0s9@outlook.com>
2024-09-09 21:23:56 +0800

    Modify clear.cpp

--- clear.cpp
+++ clear.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Eight bad happen away view quite card she.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
