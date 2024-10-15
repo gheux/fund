483dfa3d3f01d4c76cf89816b079654092ee9bc6
gheux <ialm0s9@outlook.com>
2024-10-15 11:04:27 +0800

    Modify administration.c

--- administration.c
+++ administration.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Type information section on single race seem.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
