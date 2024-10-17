f567b7c0fb811aac0ea8f3386806ef3e0cdd1a8e
gheux <ialm0s9@outlook.com>
2024-10-17 10:56:53 +0800

    Modify ground.cpp

--- ground.cpp
+++ ground.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Source he lot cell speech well model.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
