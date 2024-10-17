37d21b1a8c9579a9c8213ce3a1967a48376c053c
gheux <ialm0s9@outlook.com>
2024-10-17 10:56:52 +0800

    Modify generation.c

--- generation.c
+++ generation.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Mention guess of him still effect receive.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
