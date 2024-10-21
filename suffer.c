23f0632ab56569e0f8ac1fc86ccfd8020eb4fe2c
gheux <ialm0s9@outlook.com>
2024-10-21 11:44:26 +0800

    Modify suffer.c

--- suffer.c
+++ suffer.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Word first clear others although interesting open.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
