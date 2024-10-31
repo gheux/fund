da1d81e1fd8a9c6b4a947340d7b524a2051c042f
gheux <ialm0s9@outlook.com>
2024-10-31 13:25:47 +0800

    Modify left.cpp

--- left.cpp
+++ left.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Know law response sea approach.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
