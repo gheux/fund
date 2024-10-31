af7c9fa73acf54ce7ea8480eb45bac06c574f76d
gheux <ialm0s9@outlook.com>
2024-10-31 13:25:47 +0800

    Modify town.cpp

--- town.cpp
+++ town.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# President pass trip write among.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
