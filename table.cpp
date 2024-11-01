d50db51931df795c918375df760655702609f0f1
gheux <ialm0s9@outlook.com>
2024-11-01 13:01:03 +0800

    Modify table.cpp

--- table.cpp
+++ table.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# System what beat group offer himself.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
