0cef27d83737e64acfb25c55b81eba61a94fbd39
gheux <ialm0s9@outlook.com>
2024-11-27 13:04:16 +0800

    Modify left.cpp

--- left.cpp
+++ left.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Professional sit situation go.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
