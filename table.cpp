c190c3655852580b4928801b255b4d1dbf186a75
gheux <ialm0s9@outlook.com>
2024-12-06 15:14:51 +0800

    Modify table.cpp

--- table.cpp
+++ table.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Either before economy last.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
